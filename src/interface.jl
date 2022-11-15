b100774076fa08ae767405e278b3bb245009910e
Mathias Louboutin <mathias.louboutin@gmail.com>
2022-11-15 16:16:20 -0500

    Update interface.jl

--- src/interface.jl
+++ src/interface.jl
@@ -15,7 +15,7 @@ function forward(model::Model, q::judiVector, dobs::judiVector; options=Options(
     ts = trunc(Int, abs(src_coords[1, end] - rec_coords[1, end])/(1.5f0*dt_Comp))
     Q = qr_data(d_data, ps; ts=ts, mode=mode)
 
-    rec, eu, _ = forward(modelPy, src_coords, rec_coords, q_data, Q, options.space_order, options.isic)
+    rec, eu, _ = forward(modelPy, src_coords, rec_coords, q_data, Q, options.space_order,  options.IC == "isic")
     rec = time_resample(rec, dt_Comp, dobs.geometry)
     return judiVector(dobs.geometry, rec), Q, eu
 end
@@ -37,7 +37,7 @@ function born(model::Model, q::judiVector, dobs::judiVector, dm; options=Options
     ts = trunc(Int, abs(src_coords[1, end] - rec_coords[1, end])/(1.5f0*dt_Comp))
     Q = qr_data(d_data, ps; ts=ts)
 
-    recnl, recl, eu, _ = born(modelPy, src_coords, rec_coords, q_data, Q, options.space_order, options.isic)
+    recnl, recl, eu, _ = born(modelPy, src_coords, rec_coords, q_data, Q, options.space_order, options.IC == "isic")
     recnl = time_resample(recnl, dt_Comp, dobs.geometry)
     recl = time_resample(recl, dt_Comp, dobs.geometry)
     return judiVector(dobs.geometry, recnl), judiVector(dobs.geometry, recl), Q, eu
@@ -55,6 +55,6 @@ function backprop(model::Model, residual::judiVector, Q::Array{Float32}, eu::PyO
     rec_coords = setup_grid(residual.geometry[1], modelPy.shape)
 
     ev, _ = backprop(modelPy, d_data, rec_coords, Q, options.space_order)
-    g = combine(eu, ev, options.isic)
+    g = combine(eu, ev,  options.IC == "isic")
     return remove_padding(g.data, modelPy.padsizes; true_adjoint=options.sum_padding)
 end
